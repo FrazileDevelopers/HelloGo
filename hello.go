7e9066d455cd4707d216d9ffe11302cf1c601ad8
FrazileDevelopers <frazile.com@gmail.com>
2021-08-15 10:34:16 +0530

    Full Template Added & Bugs Fixed

--- hello.go
+++ hello.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
-	"html/template"
 )
 
 //  {{/* a comment */}}	Defines a comment
@@ -20,7 +20,21 @@ var tpl *template.Template
 var name = "Parth"
 
 func main() {
+	/* Added html files */
 	tpl, _ = tpl.ParseGlob("templates/*.html")
+
+	/* Added css Directory */
+	fs := http.FileServer(http.Dir("templates/css/"))
+	http.Handle("/css/", http.StripPrefix("/css/", fs))
+
+	/* Added js Directory */
+	js := http.FileServer(http.Dir("templates/js/"))
+	http.Handle("/js/", http.StripPrefix("/js/", js))
+
+	/* Added images Directory */
+	images := http.FileServer(http.Dir("templates/images/"))
+	http.Handle("/images/", http.StripPrefix("/images/", images))
+
 	http.HandleFunc("/", handle)
 	port := os.Getenv("PORT")
 	if port == "" {
